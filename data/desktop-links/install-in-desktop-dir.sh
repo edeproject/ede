7c245a2715388344bc3c46fa6bd0c51e4a4369a1
Sanel Zukan <karijes@users.sf.net>
2009-10-07 15:11:38 +0000

    Dumping unused default icons. Install ede-xterm.desktop only if Desktop directory is not present or it is empty. This is done by install-in-desktop-dir.sh script. With this, when EDE is started for the first time, something will be on desktop.

---
+++ data/desktop-links/install-in-desktop-dir.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Create ~/Desktop/ directory if not exists and installs target file. If directory
+# exists but is empty, files will be also installed. If directory is not empty, it will do nothing.
+
+desktop_dir="$HOME/Desktop"
+targets="ede-xterm.desktop"
+
+[ -d $desktop_dir ] || mkdir -p $desktop_dir
+
+curr_dir=`dirname $0`
+content=`ls $desktop_dir/*.desktop 2>>/dev/null`
+
+# check if directory is empty and install if it does
+if test "x$content" = "x"; then
+	for file in $targets; do
+		cp $curr_dir/$file $desktop_dir
+	done
+fi
