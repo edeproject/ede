93f8b5043a0eab4ee74f42f7f7565c8c95fda649
Sanel Zukan <karijes@users.sf.net>
2009-11-20 15:22:43 +0000

    Synced with autogen.sh from edelib.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-# to save me from typing :P
+# create aclocal.m4 file with content of given directory without 'aclocal' tool
+function aclocal_emulate() {
+	dir="$1"
+	filename="aclocal.m4"
+
+	rm -f $filename
+
+	for i in `ls $dir/*`; do
+		echo "m4_include([$i])" >> $filename
+	done
+
+	echo " " >> $filename
+}
+
 if [ "$1" = "--compile" ]; then
 	compile=1
 fi
 
-if aclocal -I m4 && autoheader && autoconf; then
+if aclocal_emulate m4 && autoheader && autoconf; then
 	echo ""
 	echo "Now run ./configure [OPTIONS]"
 	echo "or './configure --help' to see them"
